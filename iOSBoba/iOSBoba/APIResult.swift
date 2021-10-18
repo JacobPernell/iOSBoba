2bf3e0f8f61e55620161bfae0e54c709e2f43a76
Jacob Pernell <jacob.pernell@gmail.com>
2021-10-18 00:53:45 -0700

    Start of API networking code (not working yet)

---
+++ iOSBoba/iOSBoba/APIResult.swift
@@ -0,0 +1,67 @@
+//
+//  APIResponse.swift
+//  iOSBoba
+//
+//  Created by Jacob Pernell on 10/16/21.
+//
+
+import Foundation
+
+// MARK: - APIResult
+struct APIResult: Codable {
+    let total: Int
+    let businesses: [Business]
+    let region: Region
+}
+
+// MARK: - Business
+struct Business: Codable {
+    let rating: Int
+    let price, phone, id, alias: String
+    let isClosed: Bool
+    let categories: [Category]
+    let reviewCount: Int
+    let name: String
+    let url: String
+    let coordinates: Center
+    let imageURL: String
+    let location: Location
+    let distance: Double
+    let transactions: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case rating, price, phone, id, alias
+        case isClosed = "is_closed"
+        case categories
+        case reviewCount = "review_count"
+        case name, url, coordinates
+        case imageURL = "image_url"
+        case location, distance, transactions
+    }
+}
+
+// MARK: - Category
+struct Category: Codable {
+    let alias, title: String
+}
+
+// MARK: - Center
+struct Center: Codable {
+    let latitude, longitude: Double
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let city, country, address2, address3: String
+    let state, address1, zipCode: String
+
+    enum CodingKeys: String, CodingKey {
+        case city, country, address2, address3, state, address1
+        case zipCode = "zip_code"
+    }
+}
+
+// MARK: - Region
+struct Region: Codable {
+    let center: Center
+}
--- iOSBoba/iOSBoba/ViewController.swift
+++ iOSBoba/iOSBoba/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         title = "iOS Boba v1"
+        
+        getData(from: YELP_URL)
+        
     }
-
-
+    
+    // MARK: - Networking
+    func getData(from url: String) {
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(YELP_API_KEY)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            print(request)
+            if let data = data {
+                print(data)
+                if let result = try? JSONDecoder().decode(APIResult.self, from: data) {
+                    print(result)
+                } else {
+                    print("Invalid response")
+                }
+            } else if let error = error {
+                print("HTTP request failed: \(error)")
+            }
+        }
+        task.resume()
+    }
 }
-
