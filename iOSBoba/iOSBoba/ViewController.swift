d960d1ee5cc720addd7486ec8596a0c7b5e5e57f
Jacob Pernell <jacob.pernell@gmail.com>
2021-10-27 13:06:26 -0700

    Add MapKit capability and simple map view

--- iOSBoba/iOSBoba/ViewController.swift
+++ iOSBoba/iOSBoba/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
     
@@ -19,6 +20,28 @@ class ViewController: UIViewController {
         getData(from: YELP_URL, queryParams: ["location": "NYC", "term": "boba"])
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            let mapView = MKMapView()
+            
+            let leftMargin:CGFloat = 10
+            let topMargin:CGFloat = 60
+            let mapWidth:CGFloat = view.frame.size.width-20
+            let mapHeight:CGFloat = 300
+            
+            mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+            
+            mapView.mapType = MKMapType.standard
+            mapView.isZoomEnabled = true
+            mapView.isScrollEnabled = true
+            
+            // Or, if needed, we can position map in the center of the view
+            mapView.center = view.center
+            
+            view.addSubview(mapView)
+        }
     
     // MARK: - Networking
     func getData(from url: String, queryParams: [String:String]) {
